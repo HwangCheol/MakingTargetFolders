9344d857731801e122b757bf79a7a5531a313d0a
Gavin <skdog87@naver.com>
2015-04-14 23:43:24 +0900

    d

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApplication1
     static class Program
     {
         /// <summary>
-        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// 해당 응용 프로그램의 주 진입점입니다dddd
         /// </summary>
         [STAThread]
         static void Main()
